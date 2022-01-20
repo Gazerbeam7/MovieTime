fca760058835da6bbaf8b7006e442b1868a5a0f0
Rinzler <theocharron@protonmail.com>
2022-01-20 20:40:56 +0100

    added register files

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,40 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre Prénom',
+                'attr' => [
+                    'placeholder' => 'Inscrivez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre Nom',
+                'attr' => [
+                    'placeholder' => 'Incrivez votre nom de famille'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email',
+                'attr' => [
+                    'placeholder' => 'Incrivez votre email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre Mot De Passe',
+                'attr' => [
+                    'placeholder' => 'Incrivez votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmez votre mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Confirmez votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Inscription'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
